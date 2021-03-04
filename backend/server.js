1108650cc7308cf0f38abbe7b2ccf54730284274
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2021-03-04 17:33:30 +0530

    Initial Stage Deployment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import path from 'path'
 
 dotenv.config()
 
@@ -16,18 +17,31 @@ const app = express()
 
 app.use(express.json())
 
-
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
-
 app.use('/api/products', productRoutes)   //mounting
 app.use('/api/users', userRoutes)   //mounting
 app.use('/api/orders', orderRoutes) //mounting
 
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res)=> 
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')) 
+  )
+} else{
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
+
+
 app.use(notFound)
 app.use(errorHandler) 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
